d13cecf36457dc3191e4244eb46e288ced7d804c
alsakif <alsakif1.618@gmail.com>
2017-10-20 14:12:35 +0600

    acRec front-end rest of

--- ehERP/acRec.cs
+++ ehERP/acRec.cs
@@ -7,19 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace ehERP
 {
     public partial class acRec : UserControl
     {
+        MySqlConnection con = new MySqlConnection(@"datasource=localhost; port = 3306; database=eh_db; username= root; password=;");
         public acRec()
         {
             InitializeComponent();
         }
 
-        private void materialLabel15_Click(object sender, EventArgs e)
+        private void qty_TextChanged(object sender, EventArgs e)
         {
+            try
+            {
+                total.Text = (float.Parse(uPrice.Text) * float.Parse(qty.Text)).ToString(); // total = qty X unit price
+            }
+            catch (Exception x)
+            {
+                // MessageBox.Show("Errors: " + x);
+            }
+        }
+        private void recSaveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = $"insert into new_record(ItemType, PartyName, OrderNo,InvoiceNo,ItemName,UnitPrice,Quantity, Unit,Total, Remarks, Date, NPD) values" +
+                    $"('{c1.Text}','{pName.Text}','{oNo.Text}','{iNo.Text}','{prName.Text}','{uPrice.Text}','{qty.Text}','{unit.Text}','{total.Text}','{remark.Text}' @a);" +
+                    $" insert into final_rec_record(PartyName,OrderNo,InvoiceNo,Total,Advanced,Balance, Date) values " +
+                    $"('{pName.Text}','{oNo.Text}','{iNo.Text}','{total.Text}','{advanced.Text}', @a)";
+                cmd.Parameters.Add("@a", MySqlDbType.Date).Value = dateTimePicker1.Value.Date;
+                int x = cmd.ExecuteNonQuery();
 
+                if (x > 0)
+                {
+                    MessageBox.Show("Success");
+                }
+                else
+                {
+                    MessageBox.Show("there are some Errors");
+                }
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errors: " + ex);
+            }
         }
     }
 }
